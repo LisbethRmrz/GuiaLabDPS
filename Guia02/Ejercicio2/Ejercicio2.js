b47245030e6e57a24af6a91aab1f7275ec5dda98
LisbethRmrz <lisbethrmrz98@gmail.com>
2020-08-20 17:39:44 -0600

    Revert "Solucion Ejercicios 2 y 4"
    This reverts commit e94f1d959c1b258e364ef031d3b7ba335d69b0a3.

--- Guia02/Ejercicio2/Ejercicio2.js
+++
@@ -1,9 +0,0 @@
-/*Crear una interface en TypeScript a partir de este código JavaScript:
-var spiderman = {
-nombre: "Peter parket",
-poderes: ["trepar", "fuerza", "agilidad", "telas de araña"]
-};*/
-var Spiderman = {
-    nombre: "Peter Parket",
-    poderes: ["trepar", "fuerza", "agilidad", "telas de araña"]
-};
