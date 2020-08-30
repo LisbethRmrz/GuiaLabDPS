e5a7a0c00c27b0b0c5e67189c229b5a2b3713bb4
ManuelGamez <gamezmanuel.progra17@gmail.com>
2020-08-30 10:51:20 -0600

    Agregando Ejercicio1

---
+++ Guia02/Ejercicio2/Ejercicio2.js
@@ -0,0 +1,9 @@
+/*Crear una interface en TypeScript a partir de este código JavaScript:
+var spiderman = {
+nombre: "Peter parket",
+poderes: ["trepar", "fuerza", "agilidad", "telas de araña"]
+};*/
+var Spiderman = {
+    nombre: "Peter Parket",
+    poderes: ["trepar", "fuerza", "agilidad", "telas de araña"]
+};
